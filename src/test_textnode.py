a82f72ebabb94a4b63d84ec86027128a2171ca34
jakobboedker <bdkerjakob1@gmail.com>
2025-01-30 10:31:17 +0100

    made the unit test better ("i think")

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -7,7 +7,18 @@ class TestTextNode(unittest.TestCase):
     def test_eq(self):
         node = TextNode("this is textnode", TextType.BOLD, "google.dk")
         node2 = TextNode("this is textnode", TextType.BOLD, "google.dk")
-        self.assertEqual(node,node2)
+        self.assertEqual(str(node),str(node2))
+    
+    def test_not_eq(self):
+        node1 = TextNode("this is textnode 1", TextType.ITALIC, "dinmor.dk")
+        node3 = TextNode("this is textnode 3", TextType.BOLD, "google.dk")
+        self.assertNotEqual(node1,node3)
+    
+    def test_is_none(self):
+        node1 = TextNode("this is textnode 1", TextType.ITALIC, None)
+        return node1
+        
+        
 
 
 
