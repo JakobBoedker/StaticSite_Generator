da59eceac0a6f719ff2153c38ba60acb441e9328
jakobboedker <bdkerjakob1@gmail.com>
2025-02-11 11:29:22 +0100

    done with htmlnode and tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -8,6 +8,20 @@ class test_htmlnode(unittest.TestCase):
         node = HTMLNode(props={})
         return node.props_to_html() == ""
 
+    def test_propt_to_html(self):
+        node = HTMLNode(
+            props={
+                "href": "https://www.google.com",
+                "target": "_blank",
+            }
+        )
+        strnode = str(node.props_to_html())
+        self.assertEqual(strnode, ' href="https://www.google.com" target="_blank"')
+
+    def test_all_props(self):
+        node = HTMLNode()
+        return node == ""
+
 
 if __name__ == "__main__":
     unittest.main()
