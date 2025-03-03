5ff28d66edb6c9123724255511c2f3c5a08c0095
Jakob Boedker <bdkerjakob1@gmail.com>
2025-03-03 21:54:33 +0100

    block types done

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from markdown import markdown_to_blocks
+from markdown import *
 from textnode import *
 from htmlnode import *
 from markdown import *
@@ -6,6 +6,17 @@ from markdown import *
 
 def main():
     md = """
+###### test
+
+> test
+
+
+1. test
+
+
+```code test```
+
+
 This is **bolded** paragraph
 
 This is another paragraph with _italic_ text and `code` here
@@ -13,9 +24,13 @@ This is the same paragraph on a new line
 
 - This is a list
 - with items
-    """
+"""
     test = markdown_to_blocks(md)
-    print(test)
+    newlist = []
+    for block in test:
+        newlist.append(block_to_block_type(block))
+
+    print(newlist)
 
 
 main()
